43bd9ff32afbbcf917fd7f7b3971a8046dd58a19
vinyas-bharadwaj <vinyasbharadwaj101@gmail.com>
2024-09-22 00:55:48 +0530

    added login and logout features

--- src/store/models.py
+++ src/store/models.py
@@ -15,6 +15,10 @@ class Product(models.Model):
     description = models.CharField(max_length=255, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
 
+    # Sale
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
     
     def __str__(self):
         return self.name
--- src/store/views.py
+++ src/store/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 # Create your views here.
+
+# Home page
 def home(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'store/home.html', context=context)
+
+# About page
+def about(request):
+    context = {}
+    return render(request, 'store/about.html', context=context)
+
+
+# Login page
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully logged in")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error, please try again")
+            return redirect('login')
+
+    else:
+        return render(request, 'store/login.html', {})
+
+# Logout page
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Successfully logged out")
+    return redirect('home')
