8acab81099e97f6d0f399d82f74175e449711e17
vinyas-bharadwaj <vinyasbharadwaj101@gmail.com>
2024-10-13 23:15:43 +0530

    created the order model

--- src/payment/models.py
+++ src/payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 # Create your models here.
 class ShippingAddress(models.Model):
@@ -18,3 +19,31 @@ class ShippingAddress(models.Model):
     class Meta:
         # Essentially changes the display name in django-admin panel
         verbose_name_plural = "Shipping Address"
+
+# Create order model
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    shipping_address = models.TextField(max_length=15000)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"order - {str(self.id)}"
+    
+
+
+# Create order items model
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+
+
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    def __str__(self):
+        return f"order item - {str(self.id)}"
