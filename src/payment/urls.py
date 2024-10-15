2cf9634ed546c5eec9c12abe06248f9960de2e92
vinyas-bharadwaj <vinyasbharadwaj101@gmail.com>
2024-10-15 23:54:05 +0530

    added a checkout section

--- src/payment/urls.py
+++ src/payment/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('success', views.payment_success, name='payment-success')
+    path('success', views.payment_success, name='payment-success'),
+    path('checkout', views.checkout, name='checkout'),
 ]
--- src/payment/views.py
+++ src/payment/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from cart.cart import Cart
 
 # Create your views here.
 def payment_success(request):
-    return render(request, 'payment/payment-success.html', {})
+	return render(request, 'payment/payment-success.html', {})
+
+def checkout(request):
+	cart = Cart(request)
+	cart_products = cart.get_products()
+	quantities = cart.get_quantity()
+	total = cart.total()
+
+	context = {'cart_products': cart_products, 'quantities': quantities, 'total': total}
+
+	return render(request, 'payment/checkout.html', context=context)
